cde0c2c8aac667394f4209d42104d60c5073dcad
Dhruvik-Chevli <dhruvik.chevli@gmail.com>
2020-05-30 23:14:43 +0530

    Article 5

---
+++ Article5/Users/Users/TableViewController.swift
@@ -0,0 +1,76 @@
+//
+//  TableViewController.swift
+//  Users
+//
+//  Created by Dhruvik Chevli on 28/03/20.
+//  Copyright © 2020 Dhruvik Chevli. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    var users = [User](){
+        didSet{
+            DispatchQueue.main.async{
+                self.tableView.reloadData()
+            }
+        }
+    }
+
+    fileprivate func setUpTableView(){
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(UserCell.self, forCellReuseIdentifier: "Cell")
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.rowHeight = 150
+        tableView.allowsSelection = false
+        tableView.separatorStyle = UITableViewCell.SeparatorStyle.none
+        self.title = "Users"
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fetchData()
+        setUpTableView()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! UserCell
+        cell.layer.cornerRadius = 15
+        let user = users[indexPath.row]
+        cell.set(res : user)
+        return cell
+    }
+
+}
+
+extension TableViewController {
+    
+    func fetchData(){
+        Networking.sharedInstance.getUsers{[weak self] result in
+            switch result {
+            case .failure(let error):
+                print(error)
+            case .success(let usersRes):
+                self?.users=usersRes
+            }
+        }
+    }
+    
+}
+
+
+
+
+
---
+++ Article5/Users/Users/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Users
+//
+//  Created by Dhruvik Chevli on 30/05/20.
+//  Copyright © 2020 Dhruvik Chevli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
